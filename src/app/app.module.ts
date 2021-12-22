979c64075dabbcdb0fcd8b33da26a3623fb86264
Maycon <mayconrafaelcb@gmail.com>
2021-12-21 22:40:58 -0300

    Build: Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LayoutsModule} from './layouts/layouts.module';
 import { NotFoundComponent } from './shared/component/not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { NotFoundComponent } from './shared/component/not-found/not-found.compon
     BrowserModule,
     LayoutsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
